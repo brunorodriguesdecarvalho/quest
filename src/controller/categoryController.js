857a0aeffa6eb24ebd7e137e722a9021503c4727
Bruno Rodrigues de Carvalho <rodrigues_b@hotmail.com>
2022-04-17 10:49:17 -0300

    Criação de front e revisão do back para CRUD Categorias

--- src/controller/categoryController.js
+++ src/controller/categoryController.js
@@ -3,13 +3,29 @@ const express = require('express')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const authConfig = require('../config/auth.json')
-
 const Category = require('../models/categories')
+const colecaoCategoria = 'categories'
+
+const { Router } = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId
+const uri = process.env.mongoDbURI
+const { route } = require('express/lib/application');
+const { ObjectID } = require('mongodb');
+const { isValidObjectId } = require('../database');
+const authMiddleware = require('../middlewares/auth')
+const bancodedados = 'QuestDB'
+
 
 const router = express.Router()
 
+//Para Front - Tela de Super Admin para Categorias
+router.get('/', async (req, res) => {
+    res.render('categoria', {nomeJogador: nomeJogador, perfil: "SUPER ADMIN"})
+})
+
 //CREATE
-router.post('/register', async (req, res) => {
+router.post('/', async (req, res) => {
     const { categoria, free } = req.body
 
     try {
@@ -31,7 +47,7 @@ router.post('/register', async (req, res) => {
 
 
 //READ ONE
-router.get('/categoria', async (req, res) => {
+router.get('/', async (req, res) => {
     const { categoria } = req.body
 
     try {
@@ -59,7 +75,7 @@ router.get('/categorias', async (req, res) => {
         
         const allcategories = await Category.find({})
 
-        return res.send({allcategories})
+        return res.send(allcategories)
 
     } catch (err) {
         return res.status(400).send( {error: 'Falha - Leitura não realizada.'} )
@@ -69,7 +85,7 @@ router.get('/categorias', async (req, res) => {
 
 
 //UPDATE
-router.put('/categoria', async (req, res) => {
+router.put('/', async (req, res) => {
     const { oldcategoria, newcategoria, newfree } = req.body
 
    
@@ -97,24 +113,46 @@ router.put('/categoria', async (req, res) => {
 
 
 //DELETE
-router.delete('/categoria', async (req, res) => {
-    const { categoria } = req.body
-
+/* router.delete('/', async (req, res) => {
     try {
 
-        if(!categoria) {
-            res.status(401).send("Falha do Cliente: Faltou informar nome da categoria a ser deletada")
-        } else if(!await Category.findOne({ categoria: categoria }))
+        if(!req.body._id) {
+            return res.status(401).send("Falha do Cliente: Faltou informar a id da categoria a ser deletada")
+        } else if(!await Category.findOne({"_id": ObjectId(req.body._id)})) {
             return res.status(412).send({error: 'Falha - Categoria não existe.Por favor informe outra categoria.'})
-
-
-        const delcategory = await Category.deleteOne({categoria: categoria})
-
-        return res.status(200).send("Categoria deletada com sucesso")
-
+        } else {
+            const delcategory = await Category.deleteOne({"_id": ObjectId(req.body._id)})
+            return res.status(200).send("Categoria deletada com sucesso")
+        }
     } catch (err) {
         return res.status(400).send( {error: 'Falha - Delete não realizado.'} )
     }
+}) */
+
+router.delete('/', async(req, res) => {
+
+    console.log("_id:", req.body._id)
+
+    MongoClient.connect(uri, { useUnifiedTopology: true }, function (err, QuestDB) {
+        if (err) { 
+            res.status(461).send("Erro na conexão") 
+        }
+        else {
+            var dbo = QuestDB.db(bancodedados);
+            dbo.collection(colecaoCategoria).deleteOne({"_id": ObjectId(req.body._id)}, function (err, confirmacao) {
+                if (err) {
+                    console.log("Erro ao tentar deletar categoria: ", err)
+                    res.status(462).send("Impossível deletar essa categoria. Tente outro ID ou insulte o Bruno!")
+                } else if (confirmacao.deletedCount == 1) {
+                    console.log("Categoria deletada com sucesso: ",confirmacao)
+                    res.status(200).send("Categoria deletada com sucesso.")
+                } else {
+                    res.status(463).send("Deu algum outro erro! Insulte o Bruno!")
+                }
+                QuestDB.close(); 
+            })
+        }
+    })
 })
 
 
